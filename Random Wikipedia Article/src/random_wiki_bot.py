48747aa570d6ed9f53abfa9423c43ba8f24638b8
Marc Bove <marcbovegomara@gmail.com>
2020-08-23 21:43:30 +0200

    Improved bot. Added text preprocessing. Added interesting article decision.

--- Random Wikipedia Article/src/random_wiki_bot.py
+++ Random Wikipedia Article/src/random_wiki_bot.py
@@ -5,7 +5,9 @@ import re
 import time
 import yaml
 
+# Constants
 MAX_CHAR = 240
+MIN_SENTENCES = 50
 
 with open(r'../../util/keys.yaml') as f:
     try:
@@ -20,50 +22,61 @@ with open(r'../../util/keys.yaml') as f:
 
 
 def extract_text():
-    regex = re.compile('\[.]')
+    regex = re.compile('\(.+?\)|\[.+?\]')
     txt = ''
 
-    with urlopen('https://en.wikipedia.org/wiki/The_Son_of_the_Red_Corsair_(1943_film)') as response:
-    # with urlopen('https://en.wikipedia.org/wiki/Special:Random') as response:
-        url = response.geturl()
+    with urlopen('https://en.wikipedia.org/wiki/Special:Random') as response:
+        url = response.geturl()[8:]
         soup = BeautifulSoup(response, 'html.parser')
 
     for p in soup.find_all('p'):
         txt += p.getText()
 
     txt = re.sub(regex, '', txt)
+    txt = re.sub(' +', ' ', txt)
+    txt = [x for x in map(str.strip, txt.split('. ')) if x]
 
     return txt, url
 
 
 def compose_tweet(s, u):
     proto_tweet = twt = ''
-    u = '.\n' + u
+    u = '\n' + u
+    loops = 0
 
     for i in range(len(s)):
-        proto_tweet += s[i]
+        proto_tweet += s[i] + '. '
         if len(proto_tweet + u) < MAX_CHAR:
             twt = proto_tweet
+            loops += 1
         else:
-            return twt + u
+            return twt + u, loops
+
+
+def not_interesting(sentence_array):
+    return (sentence_array == '') or \
+           (sum([len(x) for x in sentence_array]) < MAX_CHAR) or \
+           (len(sentence_array[0]) > MAX_CHAR) or \
+           (len(sentence_array) < MIN_SENTENCES)
 
 
 if __name__ == '__main__':
-    sentences = ['']
-    web = ''
 
     # auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     # auth.set_access_token(access_token, access_secret)
     # api = tweepy.API(auth)
+    asd = 0
+    while asd < 15:
+        asd += 1
+        web = ''
+        sentences = ['']
 
-    while True:
-        text = ''
-        while len(text) <= MAX_CHAR:
-            text, web = extract_text()
+        while not_interesting(sentences):
+            sentences, web = extract_text()
 
-        sentences = [x for x in map(str.strip, text.split('.')) if x]
-        tweet = compose_tweet(sentences, web)
+        tweet, err = compose_tweet(sentences, web)
 
-        print(tweet)
-        time.sleep(60)
-        # api.update_status(tweet)
+        if err > 0:
+            time.sleep(10)
+            print(tweet)
+            # api.update_status(tweet)
